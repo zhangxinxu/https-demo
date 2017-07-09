94a4edfdc34530de6e40a51574f864a4b2641a03
zhangxinxu <zhangxinxu@zhangxinxu.com>
2017-07-09 23:18:13 +0800

    cache update
    cache update

--- cache/sw-demo-cache.js
+++ cache/sw-demo-cache.js
@@ -17,12 +17,14 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
-      cacheNames.map(function(cacheName) {
-        // 如果当前版本和缓存版本不一致
-        if (cacheName !== VERSION) {
-          return caches.delete(cacheName);
-        }
-      });
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
